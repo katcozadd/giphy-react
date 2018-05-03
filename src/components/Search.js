38a369e8628a9dabbf38df4c3febe7432fb9e105
katcozadd <krosecozadd@gmail.com>
2018-05-03 16:16:26 -0600

    Step 4: Wire up the search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,28 @@
 import React, {Component} from 'react'
 
 class Search extends Component {
-	// constructor() {
-	// 	super() 
-	// 	this.state = {
-	// 		search: ''
-	// 	}
-	// 	this.onInputChange = this.onInputChange.bind(this);
-	// 	this.onSearchSubmit = this.onSearchSubmit.bind(this);
-	// }
-	// onInputChange(e) {
-	// 	this.setState({
-	// 		search: e.target.value
-	// 	})
-	// }
-	// onSearchSubmit(e) {
-	// 	e.preventDefault()
-	// 	let search = this.state.search
-	// 	this.props.createSearch(search)
-	// 	this.setState({
-	// 		search: ''
-	// 	})
-	// }
+	constructor() {
+		super() 
+		this.state = {
+			search: ''
+		}
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onSearchSubmit = this.onSearchSubmit.bind(this);
+	}
+	onInputChange(e) {
+		console.log(e.target.value);
+		this.setState({
+			search: e.target.value
+		})
+	}
+	onSearchSubmit(e) {
+		e.preventDefault()
+		let search = this.state.search
+		this.props.createSearch(search)
+		this.setState({
+			search: ''
+		})
+	}
 	render(){
 		return (
 			<div>

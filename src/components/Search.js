4d4ed1e75421e691cc0dec3f5247d6f57868b70f
katcozadd <krosecozadd@gmail.com>
2018-05-03 16:06:06 -0600

    Step 3 complete, created UI for search

---
+++ src/components/Search.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+class Search extends Component {
+	// constructor() {
+	// 	super() 
+	// 	this.state = {
+	// 		search: ''
+	// 	}
+	// 	this.onInputChange = this.onInputChange.bind(this);
+	// 	this.onSearchSubmit = this.onSearchSubmit.bind(this);
+	// }
+	// onInputChange(e) {
+	// 	this.setState({
+	// 		search: e.target.value
+	// 	})
+	// }
+	// onSearchSubmit(e) {
+	// 	e.preventDefault()
+	// 	let search = this.state.search
+	// 	this.props.createSearch(search)
+	// 	this.setState({
+	// 		search: ''
+	// 	})
+	// }
+	render(){
+		return (
+			<div>
+				<h2>Search for Giphy!</h2>
+				<form onSubmit={ this.onSearchSubmit }>
+				<input 
+					onChange={ this.onInputChange }
+					placeholder='Search for cats, moms, waffles, etc!'
+					type='text'
+					value={this.state.search} />
+				<button type='submit'>Search</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default Search
